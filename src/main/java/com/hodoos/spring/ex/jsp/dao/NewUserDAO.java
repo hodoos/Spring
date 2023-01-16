afd54640094c2db05b9af13e3d76704a93ce519f
hodoos <motoboxx@naver.com>
2023-01-16 21:55:15 +0900

    [이종호] -수업내용 커밋 -500error 발생 -NewUserDAO class 파라미터 입력 오류로 인한 500error 발생해서 처리함

---
+++ src/main/java/com/hodoos/spring/ex/jsp/dao/NewUserDAO.java
@@ -0,0 +1,17 @@
+package com.hodoos.spring.ex.jsp.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.hodoos.spring.ex.jsp.model.NewUser;
+
+@Repository
+public interface NewUserDAO {
+	public NewUser selectLastUser();
+	
+	public int insertUser(
+			@Param("name") String name
+			, @Param("birthday") String birthday
+			, @Param("email") String email
+			, @Param("introduce") String introduce);
+}
